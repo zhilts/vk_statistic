86a6adeb8364e75564d546d625cd250ae727fed8
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2016-04-14 14:35:37 +0300

    Made user list uniq; added count of likes

--- vk_fetch/entities/views.py
+++ vk_fetch/entities/views.py
@@ -1,4 +1,5 @@
 # Create your views here.
+from django.db.models import Count
 from django.views.generic import ListView
 
 from entities.models import VkGroup, VkPost, VkUser
@@ -12,12 +13,19 @@ class PostListView(ListView):
     def get_queryset(self):
         return VkPost.objects.filter(group__vk_id=self.kwargs.get('group_id'))
 
+
 class UserListView(ListView):
     def get_queryset(self):
-        return VkUser.objects.filter(posts__group__vk_id=self.kwargs.get('group_id'))
+        qs = VkUser.objects \
+            .filter(posts__group__vk_id=self.kwargs.get('group_id')) \
+            .annotate(like_count=Count('id')) \
+            .distinct() \
+            .order_by('last_name', 'first_name')
+        print(qs.count())
+        return qs
 
 
 class UserLikesListView(ListView):
     def get_queryset(self):
-        post=VkPost.objects.get(pk=self.kwargs.get('post_id'))
+        post = VkPost.objects.get(pk=self.kwargs.get('post_id'))
         return post.likes.all()
