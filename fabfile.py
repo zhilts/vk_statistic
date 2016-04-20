ae6770843f201694f6a2cbad0d86baca1a685a85
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2016-04-20 15:52:08 +0300

    VK-2: Added release_develop task

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,10 @@
+import re
 from fabric.context_managers import lcd
 from fabric.decorators import task
 from fabric.operations import local
 from fabric.api import settings
+
+release_tag = 'current-release'
 
 
 class Stashed(object):
@@ -47,19 +50,33 @@ def git_current_branch():
 @task()
 def deploy():
     start_branch = git_current_branch()
-    deploy_branch = 'develop'
+    deploy_branch = 'staging'
     with Stashed():
         checkout(deploy_branch)
 
         try:
-            local('git remote add heroku git@heroku.com:vk-fetch.git ')
+            local('git remote add staging git@heroku.com:vk-fetch.git ')
         except:
             pass
         local('git fetch --tags origin')
-        local('git merge --log --no-edit tags/current-release')
-        local('git push --force --set-upstream heroku {deploy_branch}:master'.format(deploy_branch=deploy_branch))
+        local('git merge --log --no-edit tags/{release_tag}'.format(release_tag=release_tag))
+        local('git push --force --set-upstream staging {deploy_branch}:master'.format(deploy_branch=deploy_branch))
         checkout(start_branch)
 
+
+@task()
+def release_develop():
+    start_branch = git_current_branch()
+    tags_str = local('git tag -l --contains HEAD', capture=True)
+    tags = re.compile('[\w-]+', re.MULTILINE).findall(tags_str)
+    with Stashed():
+        if release_tag not in tags:
+            checkout('tags/{release_tag}'.format(release_tag=release_tag))
+            local('git fetch origin')
+            local('git merge --log --no-edit origin/develop')
+            local('git tag -a "{release_tag}" -f')
+            local('git push origin --tags -f')
+    checkout(start_branch)
 
 @task()
 def vk(command):
