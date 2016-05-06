d8857e2b2708972cbf36ce25ee20bd15479c0bed
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2016-05-06 15:19:19 +0300

    Updated Docker container provision

--- docker/tc-agent/agent-setup.sh
+++ docker/tc-agent/agent-setup.sh
@@ -12,25 +12,27 @@ fi
 if [ ! -d "$AGENT_DIR" ]; then
     cd ${HOME}
     echo "Setting up TeamCityagent for the first time..."
-    echo "Agent will be installed to ${AGENT_DIR}."
-    mkdir -p $AGENT_DIR
-    wget $TEAMCITY_SERVER/update/buildAgent.zip
-    unzip -q -d $AGENT_DIR buildAgent.zip
+    echo "Agent will be installed to $AGENT_DIR."
+    mkdir -p ${AGENT_DIR}
+    wget ${TEAMCITY_SERVER}/update/buildAgent.zip
+    unzip -q -d ${AGENT_DIR} buildAgent.zip
     rm buildAgent.zip
-    chmod +x $AGENT_DIR/bin/agent.sh
-    echo "serverUrl=${TEAMCITY_SERVER}" > $AGENT_DIR/conf/buildAgent.properties
-    echo "name=${TEAMCITY_AGENTNAME}" >> $AGENT_DIR/conf/buildAgent.properties
-    echo "workDir=../work" >> $AGENT_DIR/conf/buildAgent.properties
-    echo "tempDir=../temp" >> $AGENT_DIR/conf/buildAgent.properties
-    echo "systemDir=../system" >> $AGENT_DIR/conf/buildAgent.properties
+    chmod +x ${AGENT_DIR}/bin/agent.sh
+    echo "serverUrl=${TEAMCITY_SERVER}" > ${AGENT_DIR}/conf/buildAgent.properties
+    echo "name=${TEAMCITY_AGENTNAME}" >> ${AGENT_DIR}/conf/buildAgent.properties
+    echo "workDir=../work" >> ${AGENT_DIR}/conf/buildAgent.properties
+    echo "tempDir=../temp" >> ${AGENT_DIR}/conf/buildAgent.properties
+    echo "systemDir=../system" >> ${AGENT_DIR}/conf/buildAgent.properties
 else
     echo "Using agent at ${AGENT_DIR}."
 fi
 
 service postgresql start
 
+git config --global user.email "$TEAMCITY_AGENTNAME@$TEAMCITY_SERVER"
+git config --global user.name "$TEAMCITY_AGENTNAME"
 
-$AGENT_DIR/bin/agent.sh start
+${AGENT_DIR}/bin/agent.sh start
 
 heroku login
 
