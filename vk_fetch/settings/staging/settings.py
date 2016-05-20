f4031e1ee28ef8f5006c1a4bc22432d610aa1632
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2016-05-21 02:06:26 +0300

    Updated redis-settings

--- vk_fetch/settings/staging/settings.py
+++ vk_fetch/settings/staging/settings.py
@@ -6,4 +6,4 @@ DEBUG = False
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
-BROKER_URL = 'redis://h:p4fseodb3ur4rt7pjrnprlgsc34@ec2-54-83-33-178.compute-1.amazonaws.com:18849'
+BROKER_URL = 'redis://h:p1mvroqecpnnbf1ei2stmtnjqif@ec2-54-83-33-178.compute-1.amazonaws.com:18849'
