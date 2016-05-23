1e20525fbfdfcb66e3ae543fbd64ddd9dafbe804
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2016-05-23 13:18:12 +0300

    Fixed cirlular dependencies

--- vk_fetch/runner/fetching/__init__.py
+++ vk_fetch/runner/fetching/__init__.py
@@ -1,10 +1,9 @@
 from entities.models import VkGroup
-from runner.fetching.groups import update_group_info, process_group
-from runner.fetching.users import update_users_info, update_users_statistic
+from runner.fetching.groups import process_group
+from runner.fetching.users import update_users_info
 
 
 def process_all():
     for group in VkGroup.objects.all():
         process_group(group)
-        update_users_statistic(group)
     update_users_info()
