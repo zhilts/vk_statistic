f19df3def44bd8bebb5a15331ce69bbf324f6ccc
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2016-06-14 06:52:07 +0300

    Fixed DB credentials v2

--- vk_fetch/settings/settings.py
+++ vk_fetch/settings/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'vk-fetch',
         'USER': 'user',
-        'PASS': ''
+        'PASSWORD': ''
     }
 }
 
