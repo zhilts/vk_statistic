cc7df9322ba08f8bde6eeb4beafd38e0fa682adb
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2016-06-14 06:40:47 +0300

    Fixed DB credentials

--- vk_fetch/settings/staging/settings.py
+++ vk_fetch/settings/staging/settings.py
@@ -5,7 +5,7 @@ DEBUG = False
 DATABASES['default'].update({
     'NAME': 'vk_fetch',
     'USER': 'vk_fetch',
-    'PASS': '',
+    'PASS': '{fqRJnKb7y!R}:2.',
     'HOST': 'vk-fetch.cjovx6nrl7nk.us-west-2.rds.amazonaws.com'
 })
 
