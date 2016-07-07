22a77f1a6361dab0e6b6579dfff0adf1bafd5df0
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2016-07-07 20:27:02 +0300

    Fixed string formatting

--- vk_fetch/runner/fetching/reposts.py
+++ vk_fetch/runner/fetching/reposts.py
@@ -14,7 +14,7 @@ def fetch_reposts(post):
     for repost_data in reposts_for_post(post.post_id, post.owner_id):
         from_id = repost_data.get('from_id', -1)
         if from_id < 0:
-            print('from_id = {from_id}'.format(from_id))
+            print('from_id = {from_id}'.format(from_id=from_id))
             continue
         user, _ = VkUser.objects.get_or_create(id=from_id)
         VkRepost.objects.create(
