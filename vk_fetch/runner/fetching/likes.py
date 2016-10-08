6060e503407354c768ec9453c302a6cd9b3c45d8
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2016-10-08 13:58:42 +0300

    Reduses requests count

--- vk_fetch/runner/fetching/likes.py
+++ vk_fetch/runner/fetching/likes.py
@@ -10,6 +10,8 @@ def fetch_likes(post):
     :return:
     """
     post.likes.clear()
+    if post.likes_count == 0:
+        return
     for user_id in likes_for_post(post.post_id, post.owner_id):
         user, _ = VkUser.objects.get_or_create(id=user_id)
         post.likes.add(user)
